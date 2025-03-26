f50b5aa565f8242977d52373d43e320a173269ad
prarabdh14 <prarabdhatrey@gmail.com>
2025-03-27 04:00:16 +0530

    modifications in app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import joblib  
+import numpy as np
 
 
 app = Flask(__name__)
@@ -12,6 +13,8 @@ encoder=joblib.load('label_encoder.pkl')
 @app.route('/predict_diseasem', methods=['POST'])
 def predict_medical_term():
     try:
+        
+        
         input_synonyms = request.get_json(force=True)  
         
         if not isinstance(input_synonyms["symptoms"], list) or not input_synonyms["symptoms"]:
@@ -22,19 +25,24 @@ def predict_medical_term():
         for item in input_synonyms["symptoms"]:
             vectorized = vectorizer.transform([item])
             predict = model1.predict(vectorized)
-            prediction.append(predict)
+            prediction.append(predict[0])
         #input_vectorized = vectorizer.transform(input_synonyms["symptoms"])
         #prediction = model1.predict(input_vectorized)
         #predicted_medical_term = prediction.tolist()
-        
         #predicted_medical_term = prediction.tolist()
+        list = ['itching', 'skin_rash', 'nodal_skin_eruptions', 'continuous_sneezing', 'shivering', 'chills', 'joint_pain', 'stomach_pain', 'acidity', 'ulcers_on_tongue', 'muscle_wasting', 'vomiting', 'burning_micturition', 'spotting_ urination', 'fatigue', 'weight_gain', 'anxiety', 'cold_hands_and_feets', 'mood_swings', 'weight_loss', 'restlessness', 'lethargy', 'patches_in_throat', 'irregular_sugar_level', 'cough', 'high_fever', 'sunken_eyes', 'breathlessness', 'sweating', 'dehydration', 'indigestion', 'headache', 'yellowish_skin', 'dark_urine', 'nausea', 'loss_of_appetite', 'pain_behind_the_eyes', 'back_pain', 'constipation', 'abdominal_pain', 'diarrhoea', 'mild_fever', 'yellow_urine', 'yellowing_of_eyes', 'acute_liver_failure', 'fluid_overload', 'swelling_of_stomach', 'swelled_lymph_nodes', 'malaise', 'blurred_and_distorted_vision', 'phlegm', 'throat_irritation', 'redness_of_eyes', 'sinus_pressure', 'runny_nose', 'congestion', 'chest_pain', 'weakness_in_limbs', 'fast_heart_rate', 'pain_during_bowel_movements', 'pain_in_anal_region', 'bloody_stool', 'irritation_in_anus', 'neck_pain', 'dizziness', 'cramps', 'bruising', 'obesity', 'swollen_legs', 'swollen_blood_vessels', 'puffy_face_and_eyes', 'enlarged_thyroid', 'brittle_nails', 'swollen_extremeties', 'excessive_hunger', 'extra_marital_contacts', 'drying_and_tingling_lips', 'slurred_speech', 'knee_pain', 'hip_joint_pain', 'muscle_weakness', 'stiff_neck', 'swelling_joints', 'movement_stiffness', 'spinning_movements', 'loss_of_balance', 'unsteadiness', 'weakness_of_one_body_side', 'loss_of_smell', 'bladder_discomfort', 'foul_smell_of urine', 'continuous_feel_of_urine', 'passage_of_gases', 'internal_itching', 'toxic_look_(typhos)', 'depression', 'irritability', 'muscle_pain', 'altered_sensorium', 'red_spots_over_body', 'belly_pain', 'abnormal_menstruation', 'dischromic _patches', 'watering_from_eyes', 'increased_appetite', 'polyuria', 'family_history', 'mucoid_sputum', 'rusty_sputum', 'lack_of_concentration', 'visual_disturbances', 'receiving_blood_transfusion', 'receiving_unsterile_injections', 'coma', 'stomach_bleeding', 'distention_of_abdomen', 'history_of_alcohol_consumption', 'fluid_overload.1', 'blood_in_sputum', 'prominent_veins_on_calf', 'palpitations', 'painful_walking', 'pus_filled_pimples', 'blackheads', 'scurring', 'skin_peeling', 'silver_like_dusting', 'small_dents_in_nails', 'inflammatory_nails', 'blister', 'red_sore_around_nose', 'yellow_crust_ooze']
         
-        try:
-            encoded_terms = encoder.transform(prediction)  
-        except Exception as e:
-            return jsonify({"error": f"Encoding failed: {str(e)}"}), 500
+        input_data = np.zeros(len(list))
         
-        diseases_prediction = model2.predict(encoded_terms)
+        for symptom in prediction:
+            if symptom in list:
+                idx = list.index(symptom)
+                input_data[idx] = 1
+            else:
+                return jsonify({"error": f"Invalid input. Sympton '{symptom}' not found."}), 400
+                
+        encoded = model2.predict([input_data])[0]
+        diseases_prediction = encoder.inverse_transform([encoded])[0]
 
         return jsonify({
             "predicted_diseases": diseases_prediction.tolist()
